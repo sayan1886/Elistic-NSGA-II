494891a6e2c47e57313087b040146e1b39d66c16
Sayan Chatterjee <sayan1886@gmail.com>
2023-09-03 23:48:23 +0530

    bug fix

--- nsga2/core/population.py
+++ nsga2/core/population.py
@@ -9,9 +9,21 @@ class Population:
 
     def __iter__(self):
         return self.population.__iter__()
+    
+    def copy(self):
+        new_copy = Population()
+        new_copy.population = self.population.copy()
+        new_copy.fronts = self.fronts.copy()
+        return new_copy
 
     def extend(self, new_individuals):
         self.population.extend(new_individuals)
 
     def append(self, new_individual):
         self.population.append(new_individual)
+        
+    def remove(self, individual):
+        self.population.remove(individual)
+    
+    def unique_population(self):
+        return set(self.population)
